28b2948708cf1cc6022aad43856b933611c70a7b
Garrett <davisgarrett21@yahoo.com>
2019-07-17 17:05:57 -0500

    Initial commit. Also added a movie class that has certain traits

---
+++ MovieLibrary/Models/MovieLibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Threading.Tasks;
+using Microsoft.AspNet;
+
+namespace MovieLibrary.Models
+{
+    public class MovieLibraryContext
+    {
+        
+    }
+}
---
+++ MovieLibrary/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+
+namespace MovieLibrary
+{
+    public partial class Startup
+    {
+        public void Configuration()
+        {
+
+        }
+    }
+}
